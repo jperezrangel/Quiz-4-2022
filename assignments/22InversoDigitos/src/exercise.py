0d5936f27231ef1dc5a01eca3f0c859b7871632b
jperezrangel <88467921+jperezrangel@users.noreply.github.com>
2022-09-21 22:12:45 -0500

    Segundos ajustes al repositorio.

--- assignments/22InversoDigitos/src/exercise.py
+++ assignments/22InversoDigitos/src/exercise.py
@@ -1,26 +1,8 @@
 def invert_digits(num):
-    # Store the sign of the original number
-    negative = num < 0
-    # Make number positive
-    num = abs(num)
-    # Extract digits
-    result = 0
-    while num > 0:
-        result = result * 10 + num % 10
-        num //= 10
-    # Restore the original sign
-    if negative:
-        result *= -1
-    return result
-
+    pass
 
 def main():
-    num = int(input("Enter a number: "))
-    #escribe tu código abajo de esta línea
-    if num >= 1000000 or num <= -1000000:
-        print("Too long")
-    else:
-        print(invert_digits(num))
+    pass
 
 
 if __name__=='__main__':
